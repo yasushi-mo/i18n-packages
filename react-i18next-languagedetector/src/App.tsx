d03f052c9974ac3670c0f6743120455cb470c02c
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-12-02 15:27:54 +0900

    Add: chnageLanguage

--- react-i18next-languagedetector/src/App.tsx
+++ react-i18next-languagedetector/src/App.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   // 開発者ツールのコンソールで確認
   console.log("document.cookie:", document.cookie); // i18next=ja-JP などがあるか
@@ -10,10 +10,18 @@ function App() {
   console.log("navigator.languages:", navigator.languages);
   console.log("document.documentElement.lang:", document.documentElement.lang); // HTML タグの lang 属性
 
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div>
       <h1>{t("welcome")}</h1>
       <p>{t("greeting", { name: "太郎" })}</p>
+      <div>
+        <button onClick={() => changeLanguage("ja")}>日本語</button>
+        <button onClick={() => changeLanguage("en")}>English</button>
+      </div>
     </div>
   );
 }
